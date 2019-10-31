6ed1466a92f3a078c4c269813af33a29014fac90
Luis Febro <mr.febro@gmail.com>
2019-10-30 20:02:30 -0400

    backend - virtual fields to encrypt password

--- models/user.js
+++ models/user.js
@@ -17,13 +17,13 @@ const data = {
         required: true,
         unique: true
     },
-    hashed_password: {
-        type: String,
-        required: true
-    },
     about: {
         type: String,
         trim: true
+    },
+    hashed_password: {
+        type: String,
+        required: true
     },
     salt: String,
     role: {
@@ -35,6 +35,32 @@ const data = {
         default: []
     }
 }, { timestamps: true }
-
 const userSchema = new Schema(data);
+
+// virtual field to set an encrypted password
+userSchema
+    .virtual("password") // Here 'password' is now a property on User documents.
+    .set(function(password) {
+        this._password = password;
+        this.salt = uuidv1();
+        this.hashed_password = this.encryptPassword(password);
+    })
+    .get(function() {
+        return this._password;
+    });
+
+userSchema.methods = {
+    encryptPassword: function(password) {
+        if (!password) return "";
+        try {
+            return crypto
+                .createHmac("sha1", this.salt)
+                .update(password)
+                .digest("hex");
+        } catch (err) {
+            return "";
+        }
+    }
+};
+
 module.exports = mongoose.model('User', userSchema, collectionName);
