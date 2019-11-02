c073421a170c4cda30fa6e50f75e668d1c290991
Luis Febro <mr.febro@gmail.com>
2019-11-02 19:07:01 -0400

    navbar withRouter setting and layout

---
+++ client/src/Routes.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Signup from "./user/Signup";
+import Signin from "./user/Signin";
+import Home from "./core/Home";
+import Menu from './core/Menu';
+
+const Routes = () => {
+    return (
+        <BrowserRouter>
+            <Menu />
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/signin" exact component={Signin} />
+                <Route path="/signup" exact component={Signup} />
+            </Switch>
+        </BrowserRouter>
+    );
+};
+
+export default Routes;
